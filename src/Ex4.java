43c01fbf1e4e3350377d54352441e7dc95746eb5
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-13 20:54:00 -0300

    AdCoreção Ex4 e adição Ex5

--- src/Ex4.java
+++ src/Ex4.java
@@ -1,55 +1,26 @@
-import java.util.Random;
+import java.util.Scanner;
 
 public class Ex4 {
     public static void main(String[] args) {
-        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
         double[] temperatura = new double[12];
+        String[] mes = {"Janeiro", "Fevereiro", "Março","Abril","Maio","Junho",
+        "Julho", "Agosto", "Setembro", "Outubro","Novembro", "Dezembro"};
         double mediaano = 0;
-        int i, mes;
+        int i;
 
         for (i = 0; i < temperatura.length; i++){
-            temperatura[i] = random.nextDouble(0,35);
+            System.out.print("Digite a temperatura de " + mes[i] + ": ");
+            temperatura[i] = sc.nextDouble();
             mediaano = mediaano + temperatura[i];
         }
-        mediaano = mediaano / 12;
-
+        mediaano = mediaano / temperatura.length;
+        System.out.println(String.format("A média anual foi: %.2f",mediaano));
+        System.out.println("Os mêses acima da média foram:");
         for (i = 0; i < temperatura.length; i++){
-            if (temperatura[i] < mediaano && i == 0 ){
-                System.out.println("O mês 1 - Janeiro foi acima da média");
-                System.out.println(String.format("%.0f°C",temperatura[i]));
-            }else if (temperatura[i] < mediaano && i == 1){
-                System.out.println("O mês 2 - Fevereiro foi acima da média");
-                System.out.println(String.format("%.0f°C",temperatura[i]));
-            }else if (temperatura[i] < mediaano && i == 2){
-                System.out.println("O mês 3 - Março foi acima da média");
-                System.out.println(String.format("%.0f°C",temperatura[i]));
-            }else if (temperatura[i] < mediaano && i == 3){
-                System.out.println("O mês 4 - Abril foi acima da média");
-                System.out.println(String.format("%.0f°C",temperatura[i]));
-            }else if (temperatura[i] < mediaano && i == 4){
-            System.out.println("O mês 5 - Maio foi acima da média");
-            System.out.println(String.format("%.0f°C",temperatura[i]));
-            }else if (temperatura[i] < mediaano && i == 5){
-                System.out.println("O mês 6 - Junho foi acima da média");
-                System.out.println(String.format("%.0f°C",temperatura[i]));
-            }else if (temperatura[i] < mediaano && i == 6){
-                System.out.println("O mês 6 - Julho foi acima da média");
-                System.out.println(String.format("%.0f°C",temperatura[i]));
-            }else if (temperatura[i] < mediaano && i == 7){
-                System.out.println("O mês 8 - Agosto foi acima da média");
-                System.out.println(String.format("%.0f °C",temperatura[i]));
-            }else if (temperatura[i] < mediaano && i == 8){
-                System.out.println("O mês 9 - Setembro foi acima da média");
-                System.out.println(String.format("%.0f°C",temperatura[i]));
-            }else if (temperatura[i] < mediaano && i == 9){
-                System.out.println("O mês 10 - Outubro foi acima da média");
-                System.out.println(String.format("%.0f°C",temperatura[i]));
-            }else if (temperatura[i] < mediaano && i == 10){
-            System.out.println("O mês 11 - Novembro foi acima da média");
-            System.out.println(String.format("%.0f°C",temperatura[i]));
-            }else if (temperatura[i] < mediaano && i == 11){
-                System.out.println("O mês 12 - Dezembro foi acima da média");
-                System.out.println(String.format("%.0f°C",temperatura[i]));
+            if (temperatura[i] >= mediaano){
+                System.out.print(String.format("%-8s", mes[i]));
+                System.out.println(String.format(": %.2f°C",temperatura[i]));
             }
         }
     }
---
+++ src/Ex5.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        Random ale = new Random();
+        int[] x = new int[10];
+        int i, aux,j;
+
+        for (i = 0; i < x.length; i++){
+            x[i] = ale.nextInt(1,30);
+        }
+        System.out.println("Original:");
+        for (i  = 0; i < x.length; i++){
+            System.out.print(x[i] + " ");
+        }
+        System.out.println();
+        j = x.length - 1;
+
+        for (i = 0; i < x.length/2; i++, j--){
+            aux = x[i];
+            x[i] = x[j];
+            x[j] = aux;
+        }
+        System.out.println("Invertida:");
+        for (i  = 0; i < x.length; i++){
+            System.out.print(x[i] + " ");
+        }
+
+    }
+}
